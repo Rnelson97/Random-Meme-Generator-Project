62f09230d132177a85b518fc36e68da537012c37
Robert <robert1.nelson@umconnect.umt.edu>
2022-12-04 23:02:37 -0700

    First commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// fetch("./data.json")
+//   .then((Response) => Response.json())
+//   .then((json) => console.log(json));
+
+// const api_url = " https://api.imgflip.com/get_memes";
+// async function getMemes() {
+//   const responce = await fetch(api_url);
+//   const data = await responce.json();
+//   console.log(data.memes.length);
+// }
+
+// getMemes();
+
+// const getData = function () {
+//   console.log(jsonData);
+// };
+
+// getData();
